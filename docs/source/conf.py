856aa68716f6546181458a4bd2d7b6c133003cff
kylechampley <champley@gmail.com>
2024-02-21 08:19:49 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../src'))
 sys.path.insert(0, os.path.abspath('../../utils'))
@@ -32,15 +32,16 @@ release = 'v1.4'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon',
-    'sphinx_rtd_theme',
-    'IPython.sphinxext.ipython_console_highlighting',
-]
+#extensions = [
+#    'sphinx.ext.duration',
+#    'sphinx.ext.doctest',
+#    'sphinx.ext.autodoc',
+#    'sphinx.ext.mathjax',
+#    'sphinx.ext.napoleon',
+#    'sphinx_rtd_theme',
+#    'IPython.sphinxext.ipython_console_highlighting',
+#]
+extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon']
 
 numpydoc_show_class_members = False
 
@@ -58,8 +59,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
+html_theme = 'alabaster'
+#html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
