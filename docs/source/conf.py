20b7593b7681f21632cd6d13daa460d56f7f0683
Kyle Champley <84085392+kylechampley@users.noreply.github.com>
2024-02-21 08:44:57 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-#import sphinx_rtd_theme
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../src'))
 sys.path.insert(0, os.path.abspath('../../utils'))
@@ -41,7 +41,7 @@ release = 'v1.4'
 #    'sphinx_rtd_theme',
 #    'IPython.sphinxext.ipython_console_highlighting',
 #]
-extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon','sphinx_rtd_theme']
 
 numpydoc_show_class_members = False
 
@@ -59,8 +59,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-#html_theme = "sphinx_rtd_theme"
+#html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
