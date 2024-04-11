c774cc4c9f41c67c5044bc21bd9332c80772b303
kylechampley <champley@gmail.com>
2024-04-11 06:31:35 -0700

    started a filter sequence demo script

--- demo_leapctype/d29_filter_sequence.py
+++ demo_leapctype/d29_filter_sequence.py
@@ -1,155 +1,102 @@
-#import sys
-#sys.path.append(r'..\utils')
-#from leap_filter_sequence import *
 import matplotlib.pyplot as plt
 from leapctype import *
 leapct = tomographicModels()
 
+'''
+This script demonstrates some applications of the so-called "filter sequence" in LEAP.  The filter sequence
+is an extensible feature for which to add multiple regularization filters or "priors" to LEAP iterative reconstruction algorithms.
+
+This feature has many applications and we will slowly add these to this demon script over time.
+'''
+
+# This script can be applied to any CT geometry, but here we will demonstrate this for fan-beam data
 numCols = 512
 numAngles = 2*2*int(360*numCols/1024)
-numAngles = 100
+#numAngles = 100
 pixelSize = 0.65*512/numCols
 numRows = 1
 #leapct.set_parallelbeam(numAngles=numAngles, numRows=numRows, numCols=numCols, pixelHeight=pixelSize, pixelWidth=pixelSize, centerRow=0.5*(numRows-1), centerCol=0.5*(numCols-1), phis=leapct.setAngleArray(numAngles, 360.0))
 leapct.set_fanbeam(numAngles, numRows, numCols, pixelSize, pixelSize, 0.5*(numRows-1), 0.5*(numCols-1), leapct.setAngleArray(numAngles, 360.0), 1100, 1400)
 leapct.set_default_volume()
 g = leapct.allocateProjections() # shape is numAngles, numRows, numCols
-f_true = leapct.allocateVolume() # shape is numZ, numY, numX
+#f_true = leapct.allocateVolume() # shape is numZ, numY, numX
 
-#leapct.addObject(f_true, 4, np.array([0.0, 0.0, 0.0]), 120.0*np.array([1.0, 1.0, 1.0]), 0.02, None, None, 3)
-#leapct.addObject(f_true, 4, np.array([0.0, 0.0, 0.0]), 110.0*np.array([1.0, 1.0, 1.0]), 0.0, None, None, 3)
-#leapct.addObject(f_true, 4, np.array([0.0, 115.0, 0.0]), 2.5*np.array([1.0, 1.0, 1.0]), 0.0, None, None, 3)
+# Simulate projection data using analytic ray tracing methods
+#leapct.set_FORBILD()
+leapct.addObject(None, 4, np.array([0.0, 0.0, 0.0]), 120.0*np.array([1.0, 1.0, 1.0]), 0.02)
+for i in range(100):
+    leapct.addObject(None, 4, 100*np.array([np.random.uniform(-1,1), np.random.uniform(-1,1), 0.0]), 5.0*np.array([1.0, 1.0, 1.0]), 0.0)
+leapct.rayTrace(g)
+I_0 = 1000.0
+t = np.random.poisson(I_0*np.exp(-g))
+t[t<=1.0] = 1.0
+g[:] = -np.log(t/I_0)
 
-leapct.set_FORBILD(f_true,True)
-leapct.project(g,f_true)
-I_0 = 5000.0
-g[:] = -np.log(np.random.poisson(I_0*np.exp(-g))/I_0)
-f=leapct.FBP(g)
-#f[:] = 0.0
+# Choose which method you'd like to test
+whichMethod = 1
+#whichMethod = 2
+#whichMethod = 3
 
-#'''
-# Comment this section out to revert back to multi-GPU solution
-# with CPU-GPU data transfers to see when easy case is advantageous
-import torch
-device = torch.device("cuda:" + str(leapct.get_gpu()))
-g = torch.from_numpy(g).to(device)
-f = torch.from_numpy(f).to(device)
-#'''
+if whichMethod == 1:
+    # First, we will just try RWLS, seeded with an FBP reconstruction
+    f = leapct.FBP(g)
+    
+    # Initialize the filter sequence and give it a strength of 2e2.  Larger strengths will enforce a stronger regularization.
+    filters = filterSequence(2e2)
+    
+    # Add a Total Variation (TV) term.  Typical TV uses an L1 norm, but LEAP allows
+    # one to use any Lp norm.  Note that if p < 1, then it will be non-convex.  This is really effective in certain
+    # situations, but if you are not careful you can get trapped in a non desirable local minimum
+    filters.append(TV(leapct, delta=0.01/100.0, p=1.0))
+    
+    leapct.RWLS(g,f,200,filters,preconditioner='SQS')
+    
+elif whichMethod == 2:
+    '''
+    Now, let's try histogram sparsity. This method is simular to DART
+    This method is not convex, so you must start with a solution that is "close" before you apply this type of regularization
+    '''
 
+    f = leapct.FBP(g)
+    
+    # Initialize the filter sequence and give it a strength of 2e2.  Larger strengths will enforce a stronger regularization.
+    filters = filterSequence(2e2)
+    
+    # First add a Total Variation (TV) term.
+    filters.append(TV(leapct, delta=0.01/100.0, p=1.0))
+    
+    # Now let's add the histogram sparsity term.  Give it a low weight because this is a powerful regularizer.
+    # This regularizer requires one to specify the target values of the reconstruction.  These don't have to be perfect, but close.
+    # We know the true object should only have values of 0.0 and 0.02, so let's use these.  This regularizer can take any number of
+    # target values, i.e., "mus", but the computational complexity of the algorithm increases with more values.
+    filters.append(histogramSparsity(leapct, mus=[0.0, 0.02],weight=0.00001))
+    
+    # Run RWLS reconstruction
+    leapct.RWLS(g,f,200,filters,preconditioner='SQS')
+    
+elif whichMethod == 3:
+    '''
+    Now let's try a filter sequence with non-differentiable terms.  For this we will need to use an iterative algorithm that is not
+    based on gradients.  The only algorithm in LEAP of this type is ASDPOCS.  Note that this is not exactly mathematically valid, but 
+    it definitely works in practice.
+    '''
 
-#'''
-#leapct.BlurFilter(f,8.0)
-filters = filterSequence(2e1)
-filters.append(TV(leapct, delta=0.01/100.0, p=1.0))
-leapct.RWLS(g, f, 300, filters, preconditioner='SQS')
-leapct.MedianFilter(f,0.0,5)
+    # Start with zeros
+    f = leapct.allocate_volume()
+    
+    # Initialize the filter sequence.  Don't need to give it a strength here, because ASDPOCS does not work this way
+    filters = filterSequence()
+    
+    # Let's add a non-differentiable term, like a median filter
+    # Note that for ASDPOCS, the filters are applied in sequence, so the order does matter.
+    # With this in mind, it is better to do the median filter first, otherwise TV may preserve some of the spikes
+    filters.append(MedianFilter(leapct, 0.0, 5))
+    
+    # Let's also add a Total Variation (TV) term.
+    filters.append(TV(leapct, delta=0.01/100.0, p=1.0))
+    
+    leapct.ASDPOCS(g,f,50,10,10,filters)
 
-filters.clear()
-filters.beta *= 100.0
-filters.append(TV(leapct, delta=0.01/100.0, p=0.01))
-#filters.append(histogramSparsity(leapct, mus=[0.0, 0.021, 0.036],weight=0.00002))
-leapct.RWLS(g, f, 200, filters, preconditioner='SQS')
-
-#filters.append(histogramSparsity(leapct, mus=[0.0, 0.021, 0.036],weight=0.001))
-
-filters.clear()
-filters.append(MedianFilter(leapct, 0.0, 3))
-filters.append(histogramSparsity(leapct, mus=[0.0, 0.021, 0.036]))
-filters.append(MedianFilter(leapct, 0.0, 3))
-filters.append(TV(leapct, delta=0.01/100.0, p=1.0))
-#filters.append(TV(leapct, delta=0.01/100.0, p=1.0))
-#leapct.ASDPOCS(g,f,200,10,5,filters)
-#leapct.RWLS(g, f, 10, filters, preconditioner='SQS')
-#'''
-
-if type(f) is torch.Tensor:
-    f = f.cpu().detach().numpy()
-
-'''
-#f = leapct.allocate_volume()
-filters = filterSequence(4e1)
-filters.append(TV(leapct, delta=0.02/20.0))
-#filters.append(BilateralFilter(leapct, 4.0, 0.01))
-#filters.append(MedianFilter(leapct, 0.0, 3))
-#filters.append(histogramSparsity(leapct, mus=np.array([0.0, 0.021, 0.036]),weight=0.0001))
-#if type(filters) is filterSequence:
-#    print('YES')
-#leapct.ASDPOCS(g,f,50,2,5,filters)
-#leapct.ASDPOCS(g,f,50,1,5)
-#leapct.RWLS(g, f, 50, filters)
-filters.beta *= 1.0e-2 * 0.0
-#leapct.RDLS(g, f, 50, filters, nonnegativityConstraint=True)
-leapct.MLTR(g, f, 500, 1, filters)
-filters.append(histogramSparsity(leapct, mus=np.array([0.0, 0.021, 0.036]),weight=0.002))
-#leapct.RWLS(g, f, 50, filters)
-#leapct.ASDPOCS(g,f,50,2,2,0.02/20.0)
-
-#filter = TV(leapct, delta=0.02/20.0, alpha=1.0, f_0=f_true)
-#for n in range(50):
-#    f=filter.apply(f)
-#filter = BilateralFilter(leapct, 3.0, 0.02, 1.0)
-#filter.apply(f)
-'''
-
-'''
-denoiser = filterSequence()
-denoiser.append(TV(leapct, delta=0.02/20.0, alpha=1.0, f_0=None))
-denoiser.append(TV(leapct, delta=0.02/20.0, alpha=1.0, f_0=None))
-denoiser.append(TV(leapct, delta=0.02/20.0, alpha=1.0, f_0=None))
-denoiser.append(TV(leapct, delta=0.02/20.0, alpha=1.0, f_0=None))
-denoiser.append(BilateralFilter(leapct, 3.0, 0.02, 1.0))
-denoiser.apply(f)
-#'''
-
-'''
-denoiser = filterSequence()
-denoiser.append(histogramSparsity(leapct, mus=np.array([0.0, 0.021, 0.036])))
-denoiser.append(BlurFilter(leapct, 2.0))
-f=denoiser.apply(f)
-denoiser.clear()
-denoiser.append(histogramSparsity(leapct, mus=np.array([0.0, 0.021, 0.036])))
-f=denoiser.apply(f)
-f=denoiser.apply(f)
-f=denoiser.apply(f)
-#'''
-
-'''
-f_0 = leapct.copyData(f)
-denoiser = filterSequence()
-denoiser.append(TV(leapct, 100.0, 500.0))
-denoiser.append(histogramSparsity(leapct, mus=np.array([0.0, 0.021, 0.036]), weight=1.0))
-denoiser.append(supportSparsity(leapct, 2.0, 1.0, f_0))
-for n in range(5):
-    #f = denoiser.apply(f)
-    d = denoiser.gradient(f)
-    num = leapct.sum(d**2)
-    denom = denoiser.quadForm(f, d)
-    if denom <= 1.0e-16:
-        break
-    stepSize = num / denom
-    f -= stepSize * d
-#'''
-
-#filter = histogramSparsity(leapct, mus=np.array([0.0, 0.021, 0.036]))
-#f=filter.apply(f)
-#f=filter.apply(f)
-#leapct.MedianFilter(f,0.0,3)
-
-#filter = supportSparsity(leapct, 1.2, 1.0, f_true)
-#for n in range(50):
-#    f = filter.apply(f)
-
-#f = f-f_true
-#filter = supportSparsity(leapct, 1.2)
-#for n in range(50):
-#    f = filter.apply(f)
-#f+=f_true
-
-#filter = azimuthalFilter(leapct, 10.0, 2.0)
-#for n in range(10):
-#    f = filter.apply(f)
-#f = filter.gradient(f)
-#leapct.AzimuthalBlur(f,10.0)
-
-plt.imshow(np.squeeze(f[f.shape[0]//2,:,:]), cmap=plt.get_cmap('gray'))
+# Display the result
+plt.imshow(np.squeeze(f[f.shape[0]//2,:,:]), cmap=plt.get_cmap('gray'), vmin=0.0, vmax=0.025)
 plt.show()
