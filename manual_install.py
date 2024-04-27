c71bfeeef24f65f193c1a9bd69285d93e0107b6a
kylechampley <champley@gmail.com>
2024-04-27 15:07:22 -0700

    completed manual install script

--- manual_install.py
+++ manual_install.py
@@ -1,23 +1,29 @@
+# To use this manual install script, go to:
+# https://github.com/LLNL/LEAP/releases and download
+#   libleapct.dll if on Windows
+# or
+#   libleapct.so if on Linux
+# and copy it to the LEAP folder (the same folder as this script)
+# Then run the following command:
+# python manual_install.py
+#
+# This will copy the necessary files to your python site-packages folder
+
 import os
 import site
+import shutil
 from sys import platform as _platform
 
 if _platform == "linux" or _platform == "linux2":
-    lib_fname = 'libleapct.so'
-    
-    copy_text = 'cp ' + str(lib_fname) + ' ' + str(os.path.join(site.getsitepackages()[1], lib_fname))
-    os.system(copy_text)
+    fname_list = ['libleapct.so', 'src/leaptorch.py', 'src/leapctype.py', 'src/leap_filter_sequence.py']
     
 elif _platform == "win32":
-    lib_fname = 'libleapct.dll'
-    
-    
-    copy_text = 'copy ' + str(lib_fname) + ' ' + str(os.path.join(site.getsitepackages()[1], lib_fname))
-    os.system(copy_text)
+    fname_list = ['libleapct.dll', r'src\leaptorch.py', r'src\leapctype.py', r'src\leap_filter_sequence.py']
     
 elif _platform == "darwin":
-    lib_fname = 'libleapct.dylib'
-
-    copy_text = 'cp ' + str(lib_fname) + ' ' + str(os.path.join(site.getsitepackages()[1], lib_fname))
-    os.system(copy_text)
+    fname_list = ['libleapct.dylib', 'src/leaptorch.py', 'src/leapctype.py', 'src/leap_filter_sequence.py']
     
+dst_folder = site.getsitepackages()[1]
+print('Copying LEAP-CT files to: ' + str(dst_folder))
+for n in range(len(fname_list)):
+    shutil.copy(fname_list[n], dst_folder)
